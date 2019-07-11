add7e60411ad28d87a65612796965b51da4e8c81
小原大地 <oharadaichi@obaranoMacBook-ea.local>
2019-07-11 22:25:12 +0900

    Won/Lost name added

--- ex4.c
+++ ex4.c
@@ -17,7 +17,7 @@ int main(){
         printf("%d\n",a);
     }
     printf("Total value : %d\n",total);
-    if(total>7)printf("You won!!\n");
-    else printf("You lost\n");
+    if(total>7)printf("%s won!!\n",name);
+    else printf("%s lost\n",name);
     return 0;
 }
