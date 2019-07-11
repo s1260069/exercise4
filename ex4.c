03271fe96350d9c54e31268c5dfb6db7bcf9e777
小原大地 <oharadaichi@obaranoMacBook-ea.local>
2019-07-11 22:17:51 +0900

    First revision

---
+++ ex4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    int i,total=0;
+    srand(time(NULL));
+    printf("Rolling the Dice...\n");
+    for(i=0;i<2;i++){
+        printf("Dice %d: ",i+1);
+        int a= rand() % 6 + 1;
+        total+=a;
+        printf("%d\n",a);
+    }
+    printf("Total value : %d\n",total);
+    return 0;
+}
